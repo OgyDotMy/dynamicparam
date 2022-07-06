36ed3ef5280ddfa387265f84eb6a7eda9b7f58ad
0gy <76845848+OgyDotMy@users.noreply.github.com>
2022-07-07 02:48:52 +0800

    Create content.php

---
+++ library/content.php
@@ -0,0 +1,11 @@
+<?php 
+    function makeheading($tipe,$content){
+        echo "<h$tipe>$content</h$tipe>";
+    }
+
+    function makeCss($link){
+        echo '<link rel="stylesheet" href="https://port-3000-php-learning-whit3r0s31337431268.preview.codeanywhere.com/php-demo/public/'.$link.'.css">';
+    }
+
+?>
+
