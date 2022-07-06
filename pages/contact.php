6bfdefad151853e2eb93d0560713c5347238bf90
0gy <76845848+OgyDotMy@users.noreply.github.com>
2022-07-07 02:44:46 +0800

    Create contact.php

---
+++ pages/contact.php
@@ -0,0 +1,14 @@
+<?php include"parts/header.php";?>
+
+<!-- content -->
+<div class="content">
+        <div class="container">
+            <div class="posting">
+                <h1>Contact</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ducimus cumque recusandae esse suscipit temporibus vitae animi non tenetur fugiat. Laboriosam, modi quo nisi alias doloremque expedita molestias placeat vel eos nihil animi saepe? Excepturi provident suscipit autem ipsum harum ut? Dolorem nobis optio quaerat ea. Suscipit obcaecati consequatur hic consequuntur, exercitationem modi doloribus ad molestias deleniti mollitia dolorum inventore placeat facilis aut neque perspiciatis eius! Assumenda aliquam similique atque, expedita nemo commodi et ratione libero, inventore, doloremque dignissimos ullam minus amet veritatis dolor quidem quos necessitatibus quo consequuntur. Illum sit deleniti id ipsum cumque exercitationem amet laborum ipsam earum!</p>
+            </div>
+        </div>
+    </div>
+    <!-- end content -->
+    
+    <?php include"parts/footer.php";?>
