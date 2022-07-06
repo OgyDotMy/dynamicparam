3a677a24190a25f2d61393d42c34e24a6043deb9
OgyDotMy <76845848+OgyDotMy@users.noreply.github.com>
2022-07-07 02:40:54 +0800

    Create footer.php

---
+++ parts/footer.php
@@ -0,0 +1,11 @@
+    <!-- footer -->
+    <div class="footer">
+        <div class="container">
+            <div class="copyright">
+                2022 &copy; All Right Reserved 0gydotmy 
+            </div>
+        </div>
+    </div>
+    <!-- end footer -->
+</body>
+</html>
